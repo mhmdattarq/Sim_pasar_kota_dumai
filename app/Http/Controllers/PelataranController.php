5a75b13d7842dacbb9a9dc049f5ab9f4a34bd25c
atta <atarik.shen05@gmail.com>
2025-09-22 14:55:12 +0700

    perbaikian

--- app/Http/Controllers/PelataranController.php
+++ app/Http/Controllers/PelataranController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Pelataran;
-use App\Models\Pasar;
 use Illuminate\Support\Facades\DB;
 
 class PelataranController extends Controller
